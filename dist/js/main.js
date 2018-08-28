46e36e0e72b068400612838a6fc3213d25991c4f
Tolu <tolu58@gmail.com>
2018-08-28 19:33:40 +0100

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,21 @@
 //Select DOM items
 const menuBtn = document.querySelector('.menu-btn');
-const menutn = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('menu-branding');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 //Set Initial State of Menu
 let showMenu = false;
-menuBtn.removeEventListener('click', toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
 
         //Resset Menu State
         showMenu = true;
